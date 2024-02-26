6ebca8f4dbcea5f0950e0c5cbcd5b9ad58f0aa8f
nkadebug <66764893+nkadebug@users.noreply.github.com>
2024-02-26 11:46:09 +0530

    update

--- 0d7c0600/github.py
+++ 0d7c0600/github.py
@@ -1,19 +1,38 @@
 from requests import get
 from json import dumps
 from machine import unique_id
-from ubinascii import hexlify
-
-def getPath(path):
-    print('get : ',path)
-    res = get("https://api.github.com/repos/nkadebug/mpy/contents/"+path,headers={"User-Agent":"MPY Device"})
-    try:
-        for item in res.json():
-            if item['type']=='file':
-                print(item['path'])
-            else:
-                getPath(item['path'])
-    except:
-        print(res.text)
+from ubinascii import hexlify,a2b_base64
 
 did = hexlify(unique_id()).decode('utf-8')
+
+def getPath(path):
+    res = get("https://api.github.com/repos/nkadebug/mpy/contents/"+path,headers={"User-Agent":"MPY Device"})
+    try:
+        folder = res.json()
+    except:
+        print(res.text)
+        
+    for item in folder:
+        if item['type']=='file':
+            res = get("https://api.github.com/repos/nkadebug/mpy/contents/"+item['path'],headers={"User-Agent":"MPY Device"})
+            
+            try:
+                file = res.json()
+            except:
+                print(res.text)
+            
+            try:
+                name = item['path'].split(did+"/")[1]
+                print('writing file :',name)
+                f = open(name,'w')
+                f.write(a2b_base64(file['content']))
+                f.close()
+            except:
+                print('file write error')
+        else:
+            print('creating folder :',item['name'])
+            import os
+            os.mkdir(item['name'])
+            getPath(item['path'])
+
 getPath(did)
